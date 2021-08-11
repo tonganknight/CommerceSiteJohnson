349c4bb06476a9963c297544d2e6bd3eaf5c8a49
Malachi <malachialusa@gmail.com>
2021-08-10 18:23:21 -0600

    I added the first product page, set up an object, and itterated over it, so it would map all its elemets

--- src/components/Products1/index.js
+++ src/components/Products1/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import Breakpoint from 'react-socks';
+import {Button} from 'react-bootstrap';
 
 function Products1(){
 
     const items = [
-        { name: 'Gold Chain', 
+        { Name: 'Gold Chain', 
           Price: '$6.35',
-          URL: '#',}
+          Pic: '#',
+          Quantity: '5'},
+        { Name: 'Ruby Necklace',
+          Price: '$3.25',
+          Pic: '#',
+          Quantity: '3'},
+        { Name: 'Bronze Gem',
+          Price: '5.54',
+          Pic: '#',
+          Quantity: '3'}
+
     ]
 
     return(
@@ -17,15 +28,34 @@ function Products1(){
             </Breakpoint>
 
             <Breakpoint small down>
-                <div className="Products1Flexsm">
-                        <div className="Products1Title"> Title</div>
 
-                        <div className="Product1ProductFlex">
-                            <div className="PlaceHolderProduct"></div>
-                        </div> 
-                        <div className="PlaceHolderTitle">This is the First</div>
+                <div className="Products1Titlesm">Title</div>
+
+                    {items.map(item => (
+
+        
+            <div className="Products1Flexsm">
+                
+
+                <div className="Product1ProductFlexsm">
+                    <div className="PlaceHolderProductsm"></div>
+               </div> 
+                <div className="PlaceHolderTitlesm">{item.Name}</div>
+                <div className="PlaceholderProduct1Pricesm">{item.Price}</div>
+
+                <div class="VenmoButtonFlexsm">
+                    <a href="https://account.venmo.com/pay?recipients=Mele-tutu" className="PlaceholderVenmobuttonsm">
+                        Place Order With Venmo
+                    </a>
                 </div>
+                             </div>
+              
 
+                        ))}
+                
+   
+                
+            
             </Breakpoint>
 
 
